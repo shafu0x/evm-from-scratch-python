d50b11780eb040843e3c6bc71dc057a6dbeea2ca
shafu <selfouly@gmail.com>
2022-05-16 19:28:05 +0200

    refactor

--- memory.py
+++ memory.py
@@ -7,15 +7,12 @@ class Memory:
         if len(self.memory) == 0:
             self.memory = [0x00 for _ in range(32)]
 
-        # increase memory
+        # increase memory if not enough available
         if len(self.memory) < offset:
             while len(self.memory) < offset:
                 self.memory.append(0x00)
 
-        if type(value) == int:
-            self.memory[offset] = value
-        else:
-            self.memory[offset:len(value)] = value
+        self.memory[offset:len(value)] = value
 
     def load(self, offset):
         value = self.memory[offset:offset+32]
