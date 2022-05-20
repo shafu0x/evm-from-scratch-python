00252e0663d41e8f2f702a150dbbba1c4ee630ab
shafu <selfouly@gmail.com>
2022-05-20 11:31:59 +0200

    revert cpu changes

--- cpu.py
+++ cpu.py
@@ -11,36 +11,48 @@ from storage import *
 from memory_ops import *
 from misc import *
 from account import *
+from storage import *
+from jump import *
+from dup import *
+from swap import *
 
 
 # TODO: rename to execution engine or something
 class CPU:
-    def __init__(self, program, available_gas, calldata=[]):
+    def __init__(self, 
+                 program,
+                 balance,
+                 available_gas,
+                 calldata=[],
+                 prev_returndata=[]):
         self.pc = 0
+        self.balance = balance
         self.stack = Stack()
         self.memory = Memory()
         self.storage = Storage()
         self.program = self.load(program)
-        # TODO: add this logic
-        self.gas_used = 21000
+
+        self.stop_flag = False
 
         # inputs to program
         self.gas = available_gas # TODO
         self.calldata = calldata
 
-        # output of prev call
-        self.prev_returndata = []
+        # return of prev call
+        self.prev_returndata = prev_returndata
 
         # cache
         self.address_cache = []
 
         # output 
         self.returndata = []
-        self.logs = [] # list of log objects
+        self.logs = []
 
     def load(self, program):
         self.reset()
         return program
+
+    def append_log(log): self.logs.append(log)
 
     def reset(self):
         self.pc, self.stack = 0, Stack()
@@ -53,8 +65,6 @@ class CPU:
             raise Exception(f"{self.gas} gas left and {amount} required")
         self.gas -= amount
 
-    def append_log(log): self.logs.append(log)
-
     def access_account(self, address):
         warm = False # check if address is warm or cold
 
@@ -66,10 +76,18 @@ class CPU:
     def gas_inc(self, amount):
         self.gas += amount
 
-    def run(self):
-        op = self.program[self.pc]
+    # check if we want to run the next opcode
+    def exec_next_opcode(self):
+        if self.pc > len(self.program)-1      : return False
+        if self.stop_flag                     : return False
+        if not is_valid(self.program[self.pc]): return False
 
-        while op != STOP:
+        return True
+
+    def run(self):
+        # run until we run out of opcodes
+        while self.exec_next_opcode():
+            op = self.program[self.pc]
             print("op: ", hex(op))
             print("pc: " , self.pc)
 
@@ -104,7 +122,9 @@ class CPU:
             if op == SHR: shr(self)
 
             # MISC
-            if op == SHA3: sha3(self)
+            if op == SHA3:         sha3(self)
+            if op == SELFDESTRUCT: selfdestruct(self)
+            if op == RETURN:       _return(self)
 
             # ENV
             if op == ADDRESS:      address(self)
@@ -124,6 +144,16 @@ class CPU:
             if op == MLOAD:   mload(self)
             if op == MSTORE8: mstore8(self)
             if op == MSTORE:  mstore(self)
+
+            # MEMORY
+            if op == SLOAD:    sload(self)
+            if op == SSTORE:   sstore(self)
+
+            # JUMP
+            if op == JUMP:      jump(self)
+            if op == JUMPI:     jumpi(self)
+            if op == PC:        pc(self)
+            if op == JUMPDEST:  jumpest(self)
 
             # PUSH
             if op == PUSH1:   _push(self, 1)
@@ -159,8 +189,44 @@ class CPU:
             if op == PUSH31:  _push(self, 31)
             if op == PUSH32:  _push(self, 32)
 
-            op = self.program[self.pc]
+            # DUP
+            if op == DUP1:   _dup(self, 1)
+            if op == DUP2:   _dup(self, 2)
+            if op == DUP3:   _dup(self, 3)
+            if op == DUP4:   _dup(self, 4)
+            if op == DUP5:   _dup(self, 5)
+            if op == DUP6:   _dup(self, 6)
+            if op == DUP7:   _dup(self, 7)
+            if op == DUP8:   _dup(self, 8)
+            if op == DUP9:   _dup(self, 9)
+            if op == DUP10:  _dup(self, 10)
+            if op == DUP11:  _dup(self, 11)
+            if op == DUP12:  _dup(self, 12)
+            if op == DUP13:  _dup(self, 13)
+            if op == DUP14:  _dup(self, 14)
+            if op == DUP15:  _dup(self, 15)
+            if op == DUP16:  _dup(self, 16)
+
+            # SWAP
+            if op == SWAP1:   _swap(self, 1)
+            if op == SWAP2:   _swap(self, 2)
+            if op == SWAP3:   _swap(self, 3)
+            if op == SWAP4:   _swap(self, 4)
+            if op == SWAP5:   _swap(self, 5)
+            if op == SWAP6:   _swap(self, 6)
+            if op == SWAP7:   _swap(self, 7)
+            if op == SWAP8:   _swap(self, 8)
+            if op == SWAP9:   _swap(self, 9)
+            if op == SWAP10:  _swap(self, 10)
+            if op == SWAP11:  _swap(self, 11)
+            if op == SWAP12:  _swap(self, 12)
+            if op == SWAP13:  _swap(self, 13)
+            if op == SWAP14:  _swap(self, 14)
+            if op == SWAP15:  _swap(self, 15)
+            if op == SWAP16:  _swap(self, 16)
 
             print("gas: ", self.gas)
             print("stack: ", self.stack)
             print("memory: ", self.memory.memory)
+
+        # TODO: return things
---
+++ precompiles.py
@@ -0,0 +1,9 @@
+def ecRecover(_hash, v, r, s): pass
+def sha2_256(data): pass
+def ripemd_160(data): pass
+def identity(data): pass
+def modexp(Bsize, Esize, Msize, B, E, M): pass
+def ecAdd(x1, y1, x2, y2): pass
+def ecMul(x1, y1, s): pass
+def ecPairing(x1, y1, x2, y2): pass #TODO: arguments
+def blake2f(rounds, h, m, t, f): pass
